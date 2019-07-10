ad93fb71984930a3531b3e1de33a4d020390c316
Oscar Zambrano <oscarz.developer@gmail.com>
2019-07-09 23:14:01 -0400

    Starter files for gaysby app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Platziswag`,
+    description: `Platziswag app`,
+    author: `@oscarzz116`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
