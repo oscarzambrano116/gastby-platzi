35c3a8f42d36fabd73474368483b9a4ed9fb7474
Oscar Zambrano <oscar.zambrano@leniolabs.com>
2019-07-19 15:53:17 -0400

    Adding jumbo component

---
+++ src/components/jumbo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyledJumbo } from '../styles/components'
+
+const Jumbo = (props) => {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Find the best swag exclusive and special from Platzi</h2>
+        <small>Fancy clothes for best students</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+export default Jumbo
